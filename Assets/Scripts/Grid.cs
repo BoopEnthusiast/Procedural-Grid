4f2e1d7c5600eca790597e3939d3e295384eb30d
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-01-23 14:27:45 +1300

    Tutorial grid done!

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -36,14 +36,20 @@ public class Grid : MonoBehaviour {
 		mesh.name = "Procedural Grid";
 
 		vertices = new Vector3[(xSize + 1) * (ySize + 1)];
-
+		Vector2[] uv = new Vector2[vertices.Length];
+		Vector4[] tangents = new Vector4[vertices.Length];
+		Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
 		for (int i = 0, y = 0; y <= ySize; y++) {
 			for (int x = 0; x <= xSize; x++, i++) {
 				vertices[i] = new Vector3(x, y);
+				uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+				tangents[i] = tangent;
 			}
 		}
 
 		mesh.vertices = vertices;
+		mesh.uv = uv;
+		mesh.tangents = tangents;
 
 		int[] triangles = new int[xSize * ySize * 6];
 		for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
@@ -53,6 +59,7 @@ public class Grid : MonoBehaviour {
 				triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
 				triangles[ti + 5] = vi + xSize + 2;
 				mesh.triangles = triangles;
+				mesh.RecalculateNormals();
 				yield return wait;
 			}
 		}
